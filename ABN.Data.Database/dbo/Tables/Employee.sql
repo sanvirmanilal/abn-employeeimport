633b120b8a75e787e4f69656512580f2b8b5dfb2
Sanvir Manilal <sanvir.manilal@capgemini.com>
2019-09-08 11:30:18 +0200

    Created basic DB and DB Project

---
+++ ABN.Data.Database/dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]        INT            NOT NULL,
+    [FirstName] NVARCHAR (255) NULL,
+    [LastName]  NVARCHAR (255) NULL,
+    [Age]       INT            NULL,
+    [Salary]    MONEY          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
