d17684f04d1cb07350290dead81834063d3ebfc5
Sanvir Manilal <sanvir.manilal@capgemini.com>
2019-09-08 13:17:17 +0200

    Fixed Identity on Employee table

--- ABN.Data.Database/dbo/Tables/Employee.sql
+++ ABN.Data.Database/dbo/Tables/Employee.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Employee] (
-    [Id]        INT            NOT NULL,
+    [Id]        INT            IDENTITY(1,1) NOT NULL,
     [FirstName] NVARCHAR (255) NULL,
     [LastName]  NVARCHAR (255) NULL,
     [Age]       INT            NULL,
     [Salary]    MONEY          NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+	PRIMARY KEY CLUSTERED ([Id])
+    
 );
 
